c983ce4ccfb41ca11de7716a0fbd924746de352c
emmpann <mhdepan@gmail.com>
2022-02-26 18:08:12 +0700

    App - create first view

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int option;
+
+        System.out.println("*** DOUBLE LINKED LIST ***");
+        System.out.println("    1. Manual");
+        System.out.println("    2. Pustaka");
+        System.out.println("    3. Keluar");
+        System.out.print("    Silakan pilih [1/2/3]: ");
+        option = sc.nextInt();
+
+        switch (option){
+            case 1 : 
+                System.out.println("manual");
+                manualDoubleLinkedList();
+                break;
+            case 2 : 
+                System.out.println("pustaka");
+                libraryDoubleLinkedList();
+                break;
+            case 3 :
+                System.out.println("keluar");
+                break;
+            default : 
+                System.out.println("pilihan tidak diketahui");
+        }
+
     }
+
+    public static void manualDoubleLinkedList(){
+
+    }
+
+    public static void libraryDoubleLinkedList(){
+
+    }
+
 }
